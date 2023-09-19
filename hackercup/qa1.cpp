aa8522ec3dd0382fe13150167590441873b84d8f
yrk12 <86778498+yrk12@users.noreply.github.com>
2023-09-19 15:49:36 +0530

    Update file via Python script

--- hackercup/qa1.cpp
+++ hackercup/qa1.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+//this is qa2
 int main() {
     #ifndef ONLINE_JUDGE
-        freopen("consecutive_cuts_chapter_1_input.txt", "r" , stdin);
-        freopen("consecutive_cuts_chapter_1_output.txt", "w", stdout);
+        freopen("consecutive_cuts_chapter_2_input.txt", "r" , stdin);
+        freopen("consecutive_cuts_chapter_2_output.txt", "w", stdout);
     #endif
     int t;
     cin>>t;
@@ -13,7 +13,7 @@ int main() {
         cout<<"Case #"<<a++<<": ";
         int n, k;
         cin>>n>>k;
-        vector<int> a(n), b(n);
+        vector<int> a(n), b(n), ix;
         for(int i=0;i<n;i++){
             cin>>a[i];
         }
@@ -37,26 +37,87 @@ int main() {
         }
         else{
             int i=0, j=0;
-            while(j<n-1 && a[i]!=b[j]){
-                j++;
+            int cntin=0;
+            for(int k=n-1;k>=0;k--){
+                if(a[k]==a[i]){
+                    cntin++;
+                }
+                else{
+                    break;
+                }
             }
-            if(i==0 && j==0 && 1==k){
-                cout<<"NO"<<endl;
-            }
-            else{
-                bool f=true;
-                for(int l=0;l<n;l++){
-                    if(a[(i+l)%n]!=b[(j+l)%n]){
-                        f=false;
+            if(cntin!=n){
+                for(int k=0;k<n;k++){
+                    if(a[k]==a[i]){
+                        cntin++;
+                    }
+                    else{
+                        i=k;
                         break;
                     }
                 }
-                if(f){
-                    cout<<"YES"<<endl;
+            }
+            if(b[0]==a[0] && a[0]==b[n-1]){
+                int cntin1=0 ;
+                for(int k=n-1;k>=0;k--){
+                    if(b[k]==b[j]){
+                        cntin1++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                if(cntin1!=n){
+                    for(int k=0;k<n;k++){
+                        if(b[k]==b[i]){
+                            cntin1++;
+                        }
+                        else{
+                            j=k;
+                            break;
+                        }
+                    }
+                }
+                if(cntin1==cntin){
+                    ix.push_back(j);
+                }
+            }
+            while(j<n){
+                if(b[j]==a[0]){
+                    int cnt=0;
+                    while(j<n && b[j]==a[0]){
+                        cnt++;
+                        j++;
+                    }
+                    if(cnt==cntin){
+                        ix.push_back(j);
+                    }
                 }
                 else{
-                    cout<<"NO"<<endl;
+                    j++;
                 }
+            }
+            bool f=false;
+            while(!ix.empty()){
+                int k=i;
+                j=ix.back();
+                ix.pop_back();
+                int l=0;
+                for(;l<n;l++){
+                    if(a[(l+k)%n]!=b[(l+j)%n]){
+                        break;
+                    }
+                }
+                if(n==l){
+                    f=true;
+                    break;
+                }
+            }
+            if(f){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
             }
         }
     }
